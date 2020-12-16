a3a014f260430e850f60281ab2a680e699a15af3
Ahmed Elnoor <acc.ahmedstudio@gmail.com>
2020-12-16 20:40:52 +0200

    create intro activity

---
+++ app/src/main/java/com/a7medelnoor/quransheikalzain/ui/main/viewModel/IntroActivity.java
@@ -0,0 +1,16 @@
+package com.a7medelnoor.quransheikalzain.ui.main.viewModel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.a7medelnoor.quransheikalzain.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
